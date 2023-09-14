9bce2f28a0f8ef453e2d4a723c32e0470d4ac17e
Paweł Kowalczyk (Cumulocity IoT RdepartmentD - Externals) <pawel.kowalczyk@softwareag.com>
2023-09-14 16:14:42 +0200

    Different alarm type to disable agregation

--- main.go
+++ main.go
@@ -26,11 +26,12 @@ func checkGid(m *cumulocity.Microservice, gid string) {
 	fmt.Printf("Scanning file %s", file.Filename)
 
 	result := scanner.Scan(file.Filename)
+	message_type := fmt.Sprintf("c8y-scanner-%s", gid)
 	if result.Vulnerable {
 		message := fmt.Sprintf("File %s (id: %s) is infected! %s", path.Base(file.Filename), gid, result.Description)
-		m.RaiseMajorAlarm("c8y-scanner", message)
+		m.RaiseMajorAlarm(message_type, message)
 	} else {
 		message := fmt.Sprintf("No vulnerability found in %s", file.Filename)
-		m.RaiseEvent("c8y-scanner", message)
+		m.RaiseEvent(message_type, message)
 	}
 }
